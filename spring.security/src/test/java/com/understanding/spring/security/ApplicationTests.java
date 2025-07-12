8593e9b122857374dcd6b17f235da7560b7adca6
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-12 09:47:47 -0800

    feat: base structure.

---
+++ spring.security/src/test/java/com/understanding/spring/security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.understanding.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
