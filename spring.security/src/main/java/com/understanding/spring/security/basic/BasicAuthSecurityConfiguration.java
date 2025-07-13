7fe1c40d942fc4ef161a2173cdc614a8ed4dbcdc
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-13 03:49:36 -0800

    feat:Defining and storing user credentials in memory.

--- spring.security/src/main/java/com/understanding/spring/security/basic/BasicAuthSecurityConfiguration.java
+++ spring.security/src/main/java/com/understanding/spring/security/basic/BasicAuthSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.understanding.spring.security.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +26,13 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic(Customizer.withDefaults());
         http.csrf().disable();
         return (SecurityFilterChain) http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("user").password("{noop}vihanga").roles("USER").build();
+        UserDetails adminDetails = User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(userDetails,adminDetails);
     }
 }
