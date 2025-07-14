2c3c81a692f9e19805c439c443aef9b747e2d20e
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-13 21:13:14 -0800

    feat:Storing User Credential using JDBC.

--- spring.security/src/main/java/com/understanding/spring/security/basic/BasicAuthSecurityConfiguration.java
+++ spring.security/src/main/java/com/understanding/spring/security/basic/BasicAuthSecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.understanding.spring.security.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -10,7 +12,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -25,14 +32,24 @@ public class BasicAuthSecurityConfiguration {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
         http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
         return (SecurityFilterChain) http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource) {
         UserDetails userDetails = User.withUsername("user").password("{noop}vihanga").roles("USER").build();
-        UserDetails adminDetails = User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
+        UserDetails adminDetails = User.withUsername("admin").password("{noop}admin").roles("ADMIN","USER").build();
 
-        return new InMemoryUserDetailsManager(userDetails,adminDetails);
+        JdbcUserDetailsManager jdbcUserDetailsManager =new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(userDetails);
+        jdbcUserDetailsManager.createUser(adminDetails);
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION).build();
     }
 }
